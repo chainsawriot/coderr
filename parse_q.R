fc49399d9f2b177fd9c2369375e822b01225d8fa
chainsawriot <chainsawtiney@gmail.com>
2016-03-12 13:56:09 +0800

    Initial prototype

---
+++ parse_q.R
@@ -0,0 +1,31 @@
+codebook <- list(list(var = "Q1", prompt = "Wie geht es Ihnen?", type = "text"),
+                 list(var = "Q2", prompt = "Wie ist diem name?", type = 'text'),
+                 list(var = "Q3", prompt = "2 + 2 = ?", type = 'numeric'),
+                 list(var = "Q4", prompt = "It is good right?", type = "multi", options = c("Yes", "No"))
+                 )
+
+generate_prompt <- function(item) {
+    if (item['type'] == 'text') {
+        response <- readline(paste0(prompt = item['prompt'], " "))
+    } else if (item['type'] == 'numeric') {
+        response <- readline(paste0(prompt = item['prompt'], " "))
+        response <- as.numeric(response)
+    } else if (item['type'] == 'multi') {
+        response <- multi_handling(item)
+    }
+    return(response)
+}
+
+multi_handling <- function(item) {
+    response <- 0
+    while (response < 1 | response > length(item['options'][[1]])) {
+        cat(paste0(item['prompt'][[1]], " ("))
+        response <- readline(paste0(paste(1:length(item['options'][[1]]), item['options'][[1]], collapse = " / "), " ) "))
+        response <- as.numeric(response)
+    }
+    return(response)
+}
+
+multi_handling(list(var = "Q4", prompt = "It is good right?", type = "multi", options = c("Yes", "No")))
+
+lapply(codebook, generate_prompt)
